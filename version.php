1c1b6108e16b2d32167628d62a3638bd8b0e16ac
Kevin Leahey <kleahey@me.com>
2017-01-30 10:32:09 -0500

    Version problems

--- version.php
+++ version.php
@@ -26,16 +26,22 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = '2016120500';
+$plugin->version = 2016120500;
 
 // This is the version of Moodle this plugin requires.
-$plugin->requires = '2016112900';
+$plugin->requires = 2016112900;
 
 // This is the component name of the plugin - it always starts with 'theme_'
 // for themes and should be the same as the name of the folder.
-$plugin->component = 'theme_CAUniversity';
+$plugin->component = 'theme_cauniversity';
 
 // This is a list of plugins, this plugin depends on (and their versions).
 $plugin->dependencies = [
-    'theme_boost' => '2016120500'
+    'theme_boost' => 2016120500
 ];
+
+// This is a stable release.
+$plugin->maturity = MATURITY_STABLE;
+
+// This is the named version.
+$plugin->release = 1.0;
