ce27cc10d59460833ce58758fc8493555a7dc33e
Kevin Leahey <kleahey@me.com>
2017-01-30 10:53:11 -0500

    Inherit settings from Boost

--- lib.php
+++ lib.php
@@ -43,7 +43,7 @@ function theme_photo_get_main_scss_content($theme) {
         // We still load the default preset files directly from the boost theme. No sense in duplicating them.
         $scss .= file_get_contents($CFG->dirroot . '/theme/boost/scss/preset/plain.scss');
 
-    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_photo', 'preset', 0, '/', $filename))) {
+    } else if ($filename && ($presetfile = $fs->get_file($context->id, 'theme_cauniversity', 'preset', 0, '/', $filename))) {
         // This preset file was fetched from the file area for theme_photo and not theme_boost (see the line above).
         $scss .= $presetfile->get_content();
     } else {
--- settings.php
+++ settings.php
@@ -31,22 +31,22 @@ defined('MOODLE_INTERNAL') || die();
 if ($ADMIN->fulltree) {
 
     // Boost provides a nice setting page which splits settings onto separate tabs. We want to use it here.
-    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_photo'));
+    $settings = new theme_boost_admin_settingspage_tabs('themesettingphoto', get_string('configtitle', 'theme_cauniversity'));
 
     // Each page is a tab - the first is the "General" tab.
-    $page = new admin_settingpage('theme_photo_general', get_string('generalsettings', 'theme_photo'));
+    $page = new admin_settingpage('theme_cauniversity_general', get_string('generalsettings', 'theme_cauniversity'));
 
     // Replicate the preset setting from boost.
-    $name = 'theme_photo/preset';
-    $title = get_string('preset', 'theme_photo');
-    $description = get_string('preset_desc', 'theme_photo');
+    $name = 'theme_cauniversity/preset';
+    $title = get_string('preset', 'theme_cauniversity');
+    $description = get_string('preset_desc', 'theme_cauniversity');
     $default = 'default.scss';
 
     // We list files in our own file area to add to the drop down. We will provide our own function to
     // load all the presets from the correct paths.
     $context = context_system::instance();
     $fs = get_file_storage();
-    $files = $fs->get_area_files($context->id, 'theme_photo', 'preset', 0, 'itemid, filepath, filename', false);
+    $files = $fs->get_area_files($context->id, 'theme_cauniversity', 'preset', 0, 'itemid, filepath, filename', false);
 
     $choices = [];
     foreach ($files as $file) {
@@ -61,9 +61,9 @@ if ($ADMIN->fulltree) {
     $page->add($setting);
 
     // Preset files setting.
-    $name = 'theme_photo/presetfiles';
-    $title = get_string('presetfiles','theme_photo');
-    $description = get_string('presetfiles_desc', 'theme_photo');
+    $name = 'theme_cauniversity/presetfiles';
+    $title = get_string('presetfiles','theme_cauniversity');
+    $description = get_string('presetfiles_desc', 'theme_cauniversity');
 
     $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,
         array('maxfiles' => 20, 'accepted_types' => array('.scss')));
@@ -71,9 +71,9 @@ if ($ADMIN->fulltree) {
 
     // Variable $brand-color.
     // We use an empty default value because the default colour should come from the preset.
-    $name = 'theme_photo/brandcolor';
-    $title = get_string('brandcolor', 'theme_photo');
-    $description = get_string('brandcolor_desc', 'theme_photo');
+    $name = 'theme_cauniversity/brandcolor';
+    $title = get_string('brandcolor', 'theme_cauniversity');
+    $description = get_string('brandcolor_desc', 'theme_cauniversity');
     $setting = new admin_setting_configcolourpicker($name, $title, $description, '');
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
@@ -82,17 +82,17 @@ if ($ADMIN->fulltree) {
     $settings->add($page);
 
     // Advanced settings.
-    $page = new admin_settingpage('theme_photo_advanced', get_string('advancedsettings', 'theme_photo'));
+    $page = new admin_settingpage('theme_cauniversity_advanced', get_string('advancedsettings', 'theme_cauniversity'));
 
     // Raw SCSS to include before the content.
-    $setting = new admin_setting_configtextarea('theme_photo/scsspre',
-        get_string('rawscsspre', 'theme_photo'), get_string('rawscsspre_desc', 'theme_photo'), '', PARAM_RAW);
+    $setting = new admin_setting_configtextarea('theme_cauniversity/scsspre',
+        get_string('rawscsspre', 'theme_cauniversity'), get_string('rawscsspre_desc', 'theme_cauniversity'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
     // Raw SCSS to include after the content.
-    $setting = new admin_setting_configtextarea('theme_photo/scss', get_string('rawscss', 'theme_photo'),
-        get_string('rawscss_desc', 'theme_photo'), '', PARAM_RAW);
+    $setting = new admin_setting_configtextarea('theme_cauniversity/scss', get_string('rawscss', 'theme_cauniversity'),
+        get_string('rawscss_desc', 'theme_cauniversity'), '', PARAM_RAW);
     $setting->set_updatedcallback('theme_reset_all_caches');
     $page->add($setting);
 
